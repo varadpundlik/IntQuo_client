d098ae74a61d7563bc2c91da6e01e95e0a7ab9ae
Riddhi Kulkarni <riddhikul26@gmail.com>
2024-02-01 00:05:13 +0530

    hero section ui

--- intquo/src/app/page.tsx
+++ intquo/src/app/page.tsx
@@ -8,17 +8,30 @@ function Page({}: Props) {
   return (
     <>
       <div><Navbar/></div>
+      <div className='text-center pt-2 titlecolor'>IntQuo</div>
+      <div className='text-center pt-10 text-xl font-semibold mt-10'>Your guide to</div>
+      <div className='text-center  text-5xl pt-6 mt-15'> Interview</div>
       <div className='hellostyle'>
-        <div className='circle-container'>
-          <div className='circle'><span className='circle-text'>M</span></div>
-          <div className='circle'><span className='circle-text'>a</span></div>
-          <div className='circle'><span className='circle-text'>s</span></div>
-          <div className='circle'><span className='circle-text'>t</span></div>
-          <div className='circle'><span className='circle-text'>e</span></div>
-          <div className='circle'><span className='circle-text'>r</span></div>
-          <div className='circle'><span className='circle-text'>y</span></div>
+        {/* <div className='circle-container'>
+          <div className='circle'></div>
+          <div className='circle'></div>
+          <div className='circle'></div>
+          <div className='circle'></div>
+          <div className='circle'></div>
+          <div className='circle'></div>
+          <div className='circle'></div>
+          </div> */}
+          <div>
+          <span className='circle-text'>M</span>
+          <span className='circle-text'>a</span>
+          <span className='circle-text'>s</span>
+          <span className='circle-text'>t</span>
+          <span className='circle-text'>e</span>
+          <span className='circle-text'>r</span>
+          <span className='circle-text'>y</span>
+          </div>
         </div>
-      </div>
+      {/* </div> */}
     </>
   );
 }
