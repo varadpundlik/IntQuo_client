8719f7568b345009f4c171a8c963eeb59064b0db
Riddhi Kulkarni <riddhikul26@gmail.com>
2024-01-31 22:53:10 +0530

    created navbar and added hero element

--- intquo/src/app/page.tsx
+++ intquo/src/app/page.tsx
@@ -1,13 +1,27 @@
-"use client"
-import React from 'react'
+import React from 'react';
+import Navbar from './ui/navbar/Navbar';
+import './ui/styles.css';
 
+type Props = {};
 
-type Props = {}
-
-function page({}: Props) {
+function Page({}: Props) {
   return (
-    <div>page</div>
-  )
+    <>
+      <div><Navbar/></div>
+      <div className='hellostyle'>
+        <div className='circle-container'>
+          <div className='circle'><span className='circle-text'>M</span></div>
+          <div className='circle'><span className='circle-text'>a</span></div>
+          <div className='circle'><span className='circle-text'>s</span></div>
+          <div className='circle'><span className='circle-text'>t</span></div>
+          <div className='circle'><span className='circle-text'>e</span></div>
+          <div className='circle'><span className='circle-text'>r</span></div>
+          <div className='circle'><span className='circle-text'>y</span></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default page
+
+export default Page;
